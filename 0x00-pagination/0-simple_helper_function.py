0b76ab7e4fce30862136d0affcbac86bd404eea8
root <root@avellin.localdomain>
2024-05-17 15:31:40 +0200

    0-simple_helper_function.py

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
-def index_range(page, page_size):
+"index_range definition"
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     start_index = (page - 1) * page_size
     end_index = start_index + page_size - 1
     return start_index, end_index
