9911fbb9dda6e4e14331bd031fd40bee2e374faa
Avellin003 <uavellinbonaparte@gmail.com>
2024-05-22 07:39:50 +0200

    task_0

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,22 +1,25 @@
 #!/usr/bin/env python3
-"""Basic Cache"""
-
+""" Basic Caching """
 from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """BasicCache class that inherits from BaseCaching"""
+    """
+    Creates a file for storing cache data in value pairs"""
+
     def __init__(self):
-        super().__init__()
+        """ Constructor method"""
+        BaseCaching.__init__(self)
 
     def put(self, key, item):
-        """Add an item in the cache"""
+        """ Add an item in the cache """
         if key is None or item is None:
             pass
-        self.cache_data[key] = item
+        else:
+            self.cache_data[key] = item
 
     def get(self, key):
-        """Get an item by key"""
-        if key is None or key not in self.cache_data:
-            return None
-        return self.cache_data[key]
+        """ Get an item by key"""
+        if key is not None and key in self.cache_data.keys():
+            return self.cache_data[key]
+        return None
