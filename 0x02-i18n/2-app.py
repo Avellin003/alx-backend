b813e44bc28f5a0354220ef85c8717210c56fcdc
Avellin003 <uavellinbonaparte@gmail.com>
2024-05-28 20:49:44 +0200

    updates

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,39 +1,39 @@
 #!/usr/bin/env python3
-"""module to define the app with Babel supportg"""
-from flask import Flask, render_template, request
+"""flask babel task2"""
+from flask import (
+    Flask,
+    render_template,
+    request
+)
 from flask_babel import Babel
 
 
-# Create a Config class
 class Config(object):
-    """Define available languages"""
+    """Config class"""
     LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
-# Instantiate the Babel object
+# Flask app
 app = Flask(__name__)
-# Use Config as config for your Flask app
+# Config app
 app.config.from_object(Config)
-# Instantiate the Babel object
+# Babel app
 babel = Babel(app)
 
 
 @babel.localeselector
 def get_locale():
-    """
-    a function that determines the best match with our supported languages.
-    """
-    user_language = request.accept_languages.best_match(['en', 'fr'])
-    return user_language
+    """get locale from user request"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/', strict_slashes=False)
 def index() -> str:
-    """defining the index route"""
+    """function that renders a template"""
     return render_template('2-index.html')
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(port="5000", host="0.0.0.0", debug=True)
