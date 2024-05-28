3468a366eae0054d72b129512d30eddc2a49b6cb
Avellin003 <uavellinbonaparte@gmail.com>
2024-05-28 21:16:38 +0200

    updates

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,46 +1,43 @@
 #!/usr/bin/env python3
 """
-flask babel task2
+flask for babel and i18n
 """
-from flask import (
-    Flask,
-    render_template,
-    request
-)
+from flask import Flask, render_template, request
 from flask_babel import Babel
 
 
 class Config(object):
     """
-    a class Configures the languages to use
+    Config class for our app
     """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
-# Flask app
 app = Flask(__name__)
-# Config app
 app.config.from_object(Config)
-# Babel app
 babel = Babel(app)
 
 
 @babel.localeselector
 def get_locale():
     """
-    a function that determines the best
-    match with our supported languages.
+    a function that determines the best match with our
+    supported languages and returns the best one
     """
+
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/', strict_slashes=False)
-def index() -> str:
-    """a function that renders a template"""
+def index():
+    """
+    A function that determines the best match with our
+    supported languages and renders a template
+    """
     return render_template('2-index.html')
 
 
-if __name__ == "__main__":
-    app.run(port="5000", host="0.0.0.0", debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
