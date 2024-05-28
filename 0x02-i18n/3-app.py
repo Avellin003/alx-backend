60074a5816d211ba83709340d6bb3cb11ec2f881
Avellin003 <uavellinbonaparte@gmail.com>
2024-05-28 20:53:23 +0200

    updates

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -4,8 +4,9 @@ from flask_babel import Babel
 
 app = Flask(__name__)
 
+
 # Create a Config class
-class Config:
+class Config(object):
     # Define available languages
     LANGUAGES = ["en", "fr"]
 
@@ -13,22 +14,26 @@ class Config:
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
+
 # Use Config as config for your Flask app
 app.config.from_object(Config)
 
 # Instantiate the Babel object
 babel = Babel(app)
 
+
 @babel.localeselector
 def get_locale():
-    # Determine the user's preferred language/locale based on the request context
-    # For example, you might check the Accept-Language header or user preferences
-    user_language = request.accept_languages.best_match(app.config['LANGUAGES'])
+    """get locale from user request"""
+    languages = app.config['LANGUAGES']
+    user_language = request.accept_languages.best_match(languages)
     return user_language
 
+
 @app.route('/')
-def index():
+def index() -> str:
     return render_template('3-index.html')
+
 
 if __name__ == '__main__':
     app.run(port="5000", host="0.0.0.0", debug=True)
