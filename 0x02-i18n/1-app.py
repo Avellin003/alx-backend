4b8741ab6dc253228a69ae9399792cfe0926eaa4
Avellin003 <uavellinbonaparte@gmail.com>
2024-05-28 10:16:17 +0200

    flask_babel

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+# Instantiate the Babel object
+babel = Babel(app)
+
+# Create a Config class
+class Config:
+    # Define available languages
+    LANGUAGES = ["en", "fr"]
+
+    # Set Babel's default locale and timezone
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+# Use Config as config for your Flask app
+app.config.from_object(Config)
+# Initialize the Babel object with the Flask app
+@app.route('/')
+def index():
+    return render_template('index.html')
+if __name__ == '__main__':
+    app.run()
