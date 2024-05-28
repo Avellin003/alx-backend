81bfafbf78fe5f97fd7ebcb571c07f335f298747
Avellin003 <uavellinbonaparte@gmail.com>
2024-05-28 10:17:43 +0200

    Updates

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -20,6 +20,6 @@ app.config.from_object(Config)
 # Initialize the Babel object with the Flask app
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('0-index.html')
 if __name__ == '__main__':
     app.run()
